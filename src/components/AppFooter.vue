d5c9424cde8fcf90a17c9b3987f6ed9edbcad2c9
salvatorebriga <salvo.z2302@gmail.com>
2024-06-10 17:04:25 +0200

    added footer and main components

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,10 @@
+<script>
+  export default {
+    name: "AppFooter",
+  };
+</script>
+<template>
+  <div></div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,10 @@
+<script>
+  export default {
+    name: "AppHeader",
+  };
+</script>
+<template>
+  <div></div>
+</template>
+
+<style lang="scss" scoped></style>
