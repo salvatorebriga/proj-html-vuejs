38c65fde12b7654c5b80dd3a5149864bb93956e9
salvatorebriga <salvo.z2302@gmail.com>
2024-06-10 17:33:49 +0200

    navigation

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,7 +4,11 @@
   };
 </script>
 <template>
-  <div></div>
+  <ul>
+    <li><router-link :to="{ name: 'aboutUs' }">About Us</router-link></li>
+    <li><router-link :to="{ name: 'homepage' }">homepage</router-link></li>
+    <li><router-link :to="{ name: 'contact' }">contact</router-link></li>
+  </ul>
 </template>
 
 <style lang="scss" scoped></style>
