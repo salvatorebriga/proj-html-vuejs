89d0a7312051b6bc147d609fdef6736e5e473f6b
salvatorebriga <salvo.z2302@gmail.com>
2024-06-11 13:08:09 +0200

    added latest news section

---
+++ src/components/LatestNews.vue
@@ -0,0 +1,34 @@
+<script>
+  export default {
+    name: "latestNews",
+    data() {
+      return {};
+    },
+  };
+</script>
+
+<template>
+  <div class="container">
+    <h2>Latest news<span>.</span></h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+    <div class="line"></div>
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <img src="" alt="" />
+          <div class="label"></div>
+          <div class="info"></div>
+          <h3></h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eius
+            mas tellus dolor ...
+          </p>
+
+          <h5>READ MORE</h5>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
