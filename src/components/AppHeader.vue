4d306d9e889e9f977630edb701ff3ef8d3942c4d
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-06-11 15:22:12 +0200

    upd navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -76,11 +76,54 @@ export default {
   <header>
     <div class="container">
       <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-3">
           <img src="/img/logo-img-01.png" alt="logo" class="mt-0 logo" />
         </div>
 
-        <div class="col-md-6">
+        <div class="col-md-9 text-end">
+          <!-- navbar -->
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">Navbar</a>
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNavDarkDropdown"
+                aria-controls="navbarNavDarkDropdown"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+                <ul class="navbar-nav">
+                  <li class="nav-item dropdown">
+                    <button
+                      class="btn btn-dark dropdown-toggle"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      Dropdown
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-dark">
+                      <li><a class="dropdown-item" href="#">Action</a></li>
+                      <li>
+                        <a class="dropdown-item" href="#">Another action</a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#"
+                          >Something else here</a
+                        >
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+          <!-- navbar -->
+
           <ul class="list-unstyled list-inline">
             <li class="list-inline-item">
               <router-link :to="{ name: 'homepage' }">homepage</router-link>
