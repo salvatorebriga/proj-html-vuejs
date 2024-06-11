63ba8bea0e2fcaf13462000eefe0606325658024
salvatorebriga <salvo.z2302@gmail.com>
2024-06-11 12:14:55 +0200

    added our spec on homepage

--- src/pages/homepage.vue
+++ src/pages/homepage.vue
@@ -1,15 +1,18 @@
 <script>
   import AppJumbo from "../components/AppJumbo.vue";
+  import OurSpecialties from "../components/OurSpecialties.vue";
   export default {
     name: "homepage",
 
     components: {
       AppJumbo,
+      OurSpecialties,
     },
   };
 </script>
 <template>
   <AppJumbo />
+  <OurSpecialties />
 </template>
 
 <style lang="scss" scoped></style>
