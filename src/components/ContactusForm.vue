978590898125a3b46e067f34d05c37319e529f7a
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-06-11 15:14:55 +0200

    upd bozza form in contact

--- src/components/ContactusForm.vue
+++ src/components/ContactusForm.vue
@@ -4,47 +4,40 @@ export default {
 };
 </script>
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-4">
-        <form>
-          <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label"
-              >Email address</label
-            >
-            <input
-              type="email"
-              class="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-            />
-            <div id="emailHelp" class="form-text">
-              We'll never share your email with anyone else.
+  <div class="image">
+    <div class="container">
+      <div class="row">
+        <div class="col-5">
+          <h2>Contact us.</h2>
+          <p>Lorem ipsum dolor svit consectetur</p>
+          <form>
+            <div class="mb-3">
+              <input type="text" class="form-control" placeholder="Your Name" />
             </div>
-          </div>
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label"
-              >Password</label
-            >
-            <input
-              type="password"
-              class="form-control"
-              id="exampleInputPassword1"
-            />
-          </div>
-          <div class="mb-3 form-check">
-            <input
-              type="checkbox"
-              class="form-check-input"
-              id="exampleCheck1"
-            />
-            <label class="form-check-label" for="exampleCheck1"
-              >Check me out</label
-            >
-          </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+            <div class="mb-3">
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Your Email"
+              />
+            </div>
+            <div class="form-floating">
+              <textarea
+                class="form-control"
+                placeholder="Your Message"
+                id="floatingTextarea2"
+              ></textarea>
+            </div>
+
+            <button type="submit" class="btn">Send</button>
+          </form>
+        </div>
       </div>
     </div>
   </div>
 </template>
+<style scoped lang="scss">
+@use "../assets/scss/ContactusForm";
+</style>
