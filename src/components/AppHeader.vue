756beac6a1e8068ed077b4e188711c743668674f
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-06-11 10:10:54 +0200

    [ADD] aggiunta struttura base header top

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,7 +3,32 @@
     name: "AppHeader",
   };
 </script>
+
 <template>
+  <!-- header top -->
+  <!-- left section -->
+  <div class="top-bar">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          numero di telefono
+          <span>email</span>
+          <span>follow us</span>
+        </div>
+        <!-- left section -->
+        
+        <!-- right section -->
+        <div class="col-md-6">
+          
+          <a href="#" class="ml-3" data-toggle="modal" data-target="#loginModal">Login</a>
+                      <a href="#" class="ml-3" data-toggle="modal" data-target="#registerModal">Register</a>
+        </div>
+        <!-- /right section -->
+
+      </div>
+    </div>
+  </div>
+  <!-- /header top -->
   <ul>
     <li><router-link :to="{ name: 'homepage' }">homepage</router-link></li>
     <li><router-link :to="{ name: 'aboutUs' }">About Us</router-link></li>
