305029480100fa7cab1f97327bd0fd3232c88b5e
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-06-11 17:15:38 +0200

    upd prima bozza componente contattiCard

--- src/components/ContactusContatti.vue
+++ src/components/ContactusContatti.vue
@@ -1,8 +1,57 @@
 <script>
+import Card from "./ContattiCard.vue";
 export default {
   name: "ContactusContatti",
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      cities: [
+        {
+          name: "LA",
+          indirizzo: "457 BlgBlue Street, NY 10013",
+          telefono: "(111) 1234-5678",
+          email: "everlead1@qodeinteractive.com",
+        },
+        {
+          name: "NY",
+          indirizzo: "457 BlgBlue Street, NY 10013",
+          telefono: "(222) 1234-5678",
+          email: "everlead2@qodeinteractive.com",
+        },
+        {
+          name: "NO",
+          indirizzo: "289 O'Keefe Ave, N= 70112",
+          telefono: "(333) 1234-5678",
+          email: "everlead3@qodeinteractive.com",
+        },
+      ],
+    };
+  },
 };
 </script>
 <template>
-  <h3>PROVA CONTATTI</h3>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h2>Contact us <span>.</span></h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        <div class="line"></div>
+      </div>
+
+      <div class="col-4" v-for="city in cities">
+        <Card
+          :name="city.name"
+          :indirizzo="city.indirizzo"
+          :telefono="city.telefono"
+          :email="city.email"
+        />
+      </div>
+    </div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+@use "../assets/scss/ContactusContatti";
+</style>
---
+++ src/components/ContattiCard.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: "Card",
+  props: ["name", "indirizzo", "telefono", "email"],
+};
+</script>
+
+<template>
+  <ul>
+    <li>
+      {{ name }}
+    </li>
+    <li>
+      {{ indirizzo }}
+    </li>
+    <li>
+      {{ telefono }}
+    </li>
+    <li>
+      {{ email }}
+    </li>
+  </ul>
+</template>
