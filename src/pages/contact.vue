4c5ee12019e2cdc0dcb53b9fcde60dcd4b39f655
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-06-11 23:56:39 +0200

    upd prima bozza mappa

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,15 +1,18 @@
 <script>
 import ContactusForm from "../components/ContactusForm.vue";
 import ContactusContatti from "../components/ContactusContatti.vue";
+import ContactMappa from "../components/ContactMappa.vue";
 export default {
   name: "contact",
   components: {
     ContactusForm,
     ContactusContatti,
+    ContactMappa,
   },
 };
 </script>
 <template>
+  <ContactMappa />
   <ContactusContatti />
   <ContactusForm />
 </template>
