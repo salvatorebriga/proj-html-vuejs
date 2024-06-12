cb14f7fefe6897376afe08561e63bc7ffbdd0bd5
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-06-12 22:52:40 +0200

    upd aggiunto carosello completo in founder

--- src/components/AppTeam.vue
+++ src/components/AppTeam.vue
@@ -7,7 +7,7 @@ export default {
 <template>
   <div class="container-fluid mb-5 mr-bg-color">
     <div class="row">
-      <div class="col-sm-4">
+      <div class="col-sm-4 position-relative">
         <div
           id="carouselExampleControlsNoTouching"
           class="carousel slide"
@@ -15,21 +15,18 @@ export default {
         >
           <div class="carousel-inner">
             <div class="carousel-item active">
-              <img
-                src="/img/our-gatherings-01.jpg"
-                class="d-block w-100"
-                alt="..."
-              />
+              <img src="/img/our-gatherings-01.jpg" class="d-block" alt="..." />
             </div>
             <div class="carousel-item">
-              <img src="/img/latestNews2.jpg" class="d-block w-100" alt="..." />
+              <img src="/img/latestNews2.jpg" class="d-block" alt="..." />
             </div>
             <div class="carousel-item">
-              <img src="/img/founder.jpg" class="d-block w-100" alt="..." />
+              <img src="/img/founder.jpg" class="d-block" alt="..." />
             </div>
           </div>
+          <!-- Posizionamento corretto dei pulsanti all'interno del carousel -->
           <button
-            class="carousel-control-prev"
+            class="carousel-control-prev carousel-nav"
             type="button"
             data-bs-target="#carouselExampleControlsNoTouching"
             data-bs-slide="prev"
@@ -38,7 +35,7 @@ export default {
             <span class="visually-hidden">Previous</span>
           </button>
           <button
-            class="carousel-control-next"
+            class="carousel-control-next carousel-nav"
             type="button"
             data-bs-target="#carouselExampleControlsNoTouching"
             data-bs-slide="next"
@@ -48,10 +45,10 @@ export default {
           </button>
         </div>
       </div>
-      <div class="col-sm-8">
+      <div class="col-sm-8 mr-p-50">
         <div class="content text-align-left">
-          <h3>giovanni</h3>
-          <span>founder</span>
+          <h3>Jason Bickford</h3>
+          <span class="role">Founder and Executive Director</span>
           <div class="line"></div>
           <p class="mb-5">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat a
@@ -59,15 +56,18 @@ export default {
             assumenda provident quia iste tempora pariatur obcaecati aspernatur
             aperiam quis? Cumque, commodi in!
           </p>
-          <h2 class="firma">giovanni</h2>
-          <a href="#"><i class="fa-brands fa-linkedin-in social"></i></a>
-          <a href="#"><i class="fa-brands fa-facebook-f social"></i></a>
-          <a href="#"><i class="fa-brands fa-twitter social"></i></a>
+          <h2 class="firma">Jason Bickford</h2>
+          <div class="social-icons">
+            <a href="#"><i class="fa-brands fa-linkedin-in social"></i></a>
+            <a href="#"><i class="fa-brands fa-facebook-f social"></i></a>
+            <a href="#"><i class="fa-brands fa-twitter social"></i></a>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
 <style lang="scss" scoped>
 .mr-bg-color {
   background-color: #fafafa;
@@ -76,6 +76,10 @@ export default {
 
 .content {
   background-color: #ffffff;
+  padding: 89px 60px 110px 61px;
+  position: relative;
+  bottom: 20px;
+  right: 100px;
 }
 
 .line {
@@ -95,6 +99,10 @@ export default {
   text-align: center;
   padding-top: 6px;
   margin-right: 10px;
+
+  &:hover {
+    background-color: black;
+  }
 }
 
 .firma {
@@ -104,5 +112,60 @@ export default {
   font-weight: 300;
   color: #ff4612;
   margin: -19px 0 -41px 62.7%;
+}
+
+.mr-p-50 {
+  padding: 50px;
+}
+
+.position-relative {
+  position: relative;
+}
+
+.carousel-nav {
+  position: absolute;
+  bottom: -300px;
+  left: 10px;
+  z-index: 10;
+  border: none;
+  outline: none;
+  color: #ff4612;
+}
+
+.carousel-control-next {
+  left: 50px;
+}
+
+.carousel-control-prev-icon,
+.carousel-control-next-icon {
+  background-color: #ff4612;
+  width: 30px;
+  height: 30px;
+}
+
+img {
+  width: 700px;
+  height: 500px;
+}
+
+h3 {
+  font-size: 35px;
+  line-height: 1.257em;
+}
+
+.role {
+  display: block;
+  font-size: 15px;
+  font-weight: 300;
+  line-height: 24px;
+  margin-top: 4px;
+  color: #606060;
+}
+
+p {
+  font-size: 18px;
+  font-weight: 300;
+  line-height: 1.556em;
+  color: #606060;
 }
 </style>
