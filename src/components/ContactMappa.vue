ef1c39966c4b4561138e1702652c4d6b0985fdb4
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-06-13 14:18:51 +0200

    fix pulizia componente ContactMappa

--- src/components/ContactMappa.vue
+++ src/components/ContactMappa.vue
@@ -4,24 +4,10 @@ export default {
 };
 </script>
 <template>
-  <div>
-    <iframe
-      src="https://snazzymaps.com/embed/616067"
-      width="100%"
-      height="650px"
-      style="border: none"
-    ></iframe>
-  </div>
+  <iframe
+    src="https://snazzymaps.com/embed/616067"
+    width="100%"
+    height="650px"
+    style="border: none"
+  ></iframe>
 </template>
-<style scoped lang="scss">
-div {
-  padding: 1px;
-}
-iframe {
-  &:focus {
-    border: none;
-    box-shadow: none;
-    box-decoration-break: none;
-  }
-}
-</style>
